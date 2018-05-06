85df64eb3aeef48f194f0341ad68739de56b0ea5
Kris Penney <hello@krispenney.me>
2018-05-06 00:23:55 -0400

    Integrate old script into new

--- setup/shell.sh
+++ setup/shell.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-
+echo "Install oh-my-zsh"
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+mkdir "$ZSH"/z
+wget https://raw.githubusercontent.com/rupa/z/master/z.sh -P "$ZSH"/z
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
